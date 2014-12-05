5f66d2eb1a0b90e95dff4c33d0329c0e0fb13a13
cchehoud <cchehoud@acd2042c-187b-11de-8279-d8d34a11d372>
2014-12-05 15:13:05 +0000

    Implemantation completed and tested.

--- contigs2circular.py
+++ contigs2circular.py
@@ -3,18 +3,19 @@
 import argparse
 
 def extract_circularity (filein):
-    contigs2circularity = {}
+    circularcontigs = []
+    
     for line in filein:
         line = line.rstrip('\n')
         if line.startswith(">"):
             values = line.split(" ")
             contig_name = values[0].split(">")[1]
-            contigs2circularity[contig_name] = 'circular'
-    return(contigs2circularity)
+            circularcontigs.append(contig_name)
+    return(set(circularcontigs))
     
 if __name__ == '__main__':
     '''
-    This program will the output of the circularity program (fasta file of circular contigs) and return a hash of the contigs and their circularity. 
+    This program will the output of the circularity program (fasta file of circular contigs) and return a set of the circular contigs.
     '''
 
     parser = argparse.ArgumentParser(description='Contig Name to Circularity.')
@@ -23,6 +24,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print "Extracting information from contig file:"
-    extract_circularity(args.circularContigsFile)
+    circularcontigs = extract_circularity(args.circularContigsFile)
+    
+    print "circularContigNames"
+    for contig in circularcontigs:
+        print contig
 
