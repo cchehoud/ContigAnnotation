d4a775735c08ea692f74edb33e58353c21282b8f
cchehoud <cchehoud@acd2042c-187b-11de-8279-d8d34a11d372>
2014-12-05 14:55:28 +0000

    Implemantation completed and tested.

--- contigs2length.py
+++ contigs2length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import sys
 
 def extract_name_length_readcount (filein):
     contigs2length = {}
@@ -9,6 +10,9 @@ def extract_name_length_readcount (filein):
         line = line.rstrip('\n')
         if line.startswith(">"):
             values = line.split(" ")
+            if not len(values) == 3:
+                    print("ERROR: Contig file is not in the correct format.")
+                    sys.exit(1)
             contig_name = values[0].split(">")[1]
             length = values[1].split("length_")[1]
             read_count = values[2].split("read_count_")[1]
@@ -18,7 +22,7 @@ def extract_name_length_readcount (filein):
     
 if __name__ == '__main__':
     '''
-    This program will take a FASTA file of contigs (outputed by glimmer) and extract the name, length, and read counts. 
+    This program will take a FASTA file of contigs (outputed by IDBA-UD) and extract the name, length, and read counts. The format of the identifier is as follows ">ContigName length read_count". For example: ">contig-95_2 length_33501 read_count_391116".
     '''
 
     parser = argparse.ArgumentParser(description='Contig Names to Length and Read Count.')
@@ -27,6 +31,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print "Extracting information from contig file:"
-    extract_name_length_readcount(args.contigFile)
-
+    (contigs2length,contigs2readcount) = extract_name_length_readcount(args.contigFile)
+    print "ContigName\tLength\tReadCount"
+    for contig, length in contigs2length.items():
+        readcount = contigs2readcount[contig]
+        print contig + "\t" + length + "\t" + readcount
+        
