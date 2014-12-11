3419705961483aa2107ddbf996af1445c72324f6
cchehoud <cchehoud@acd2042c-187b-11de-8279-d8d34a11d372>
2014-12-11 20:42:57 +0000

    Correct file handles v file names.

--- contigAnnotation.py
+++ contigAnnotation.py
@@ -18,9 +18,9 @@ def run_glimmer(contig_file):
 def run_blast_viraldb(ORF_file, viral_Blast_file, ref_viralDB):
     subprocess.check_call(["bash", "blast.viral.families.sh", ORF_file, viral_Blast_file, ref_viralDB])
 
-def extract_annotations(contig_file_fh, circle_file_name, orf_file_fh, blast_file_fh):
+def extract_annotations(contig_file_fh, circle_file_fh, orf_file_fh, blast_file_fh):
     (c2length, c2readcount) = contigs2length.extract_name_length_readcount(contig_file_fh)
-    c_circular = contigs2circular.extract_circularity(circle_file_name)
+    c_circular = contigs2circular.extract_circularity(circle_file_fh)
     c2ORFs = contigs2ORFs.extract_ORF_counts(orf_file_fh)
     c2viralORFs = contig2ViralORFs.extract_ORF_counts(blast_file_fh)
     c2familyName = contig2ViralFamily.extract_family_name(blast_file_fh)
@@ -74,18 +74,20 @@ if __name__ == '__main__':
         kMerMax = 1000
         min_len = 3500
         circular.find_circular_by_kmer(args.contigFile, circle_fh, kMerMin, kMerMax, min_len)
-
+        circle_fh.close()
+        circle_fh = open(circle_file, 'r')
+        
         print "Running Glimmer:" 
-        run_glimmer(args.contigFile)
+#        run_glimmer(args.contigFile)
 
         ORF_file = basename + ".fastp"
         ORF_file_fh = open(ORF_file, 'r')
         viral_Blast_file = basename + "_viral_blastout.txt"
         print "Running BLAST on viral db:"
-        run_blast_viraldb(ORF_file, viral_Blast_file, args.ref_viral)
+#        run_blast_viraldb(ORF_file, viral_Blast_file, args.ref_viral)
         viral_Blast_fh = open(viral_Blast_file, 'r')
         
-        table = extract_annotations(args.contigFile, circle_file, ORF_file_fh, viral_Blast_fh)
+        table = extract_annotations(args.contigFile, circle_fh, ORF_file_fh, viral_Blast_fh)
 
         if (args.outputFile): 
             output = open(args.outputFile, 'w')
