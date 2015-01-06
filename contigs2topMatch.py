cce386c3534463e13b0a649108d84f55dbe86e76
cchehoud <cchehoud@acd2042c-187b-11de-8279-d8d34a11d372>
2015-01-06 23:01:20 +0000

    Blast outfmt 6 table => contig to top match map.

---
+++ contigs2topMatch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import argparse
+
+def extract_counts (filein):
+    filein.seek(0)
+    contigs2match = {}
+    for line in filein:
+        line = line.rstrip()
+        values = line.split("\t")
+        contig = values[0]
+        match = values[1]
+        if not (contig in contigs2match):
+            contigs2match[contig] = match
+    return(contigs2match)
+            
+if __name__ == '__main__':
+    '''
+    This program will take BLAST output file (format 6 with qseqid and stitle) and return a hash with the query and the top blast match.
+    '''
+
+    parser = argparse.ArgumentParser(description='Contig Name to Top Match.')
+    parser.add_argument('-i', '--input', dest='blastFile', required=True, type=file,
+                         help='Required input: BLAST file in format 6 with qseqid and stitle.') 
+
+    args = parser.parse_args()
+
+    print "Extracting information from BLAST file:"
+    contigs2count = extract_counts(args.blastFile)
+
+    print "ContigName\tTopMatch"
+    for contig, match in contigs2count.items():
+        print contig + "\t" + str(match)
