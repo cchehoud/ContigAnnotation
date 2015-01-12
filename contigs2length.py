817a98032f1eb89f1b4ff9d0d4182c58cfc339af
Christel <cchehoud@gmail.com>
2015-01-12 14:16:35 -0500

    Counts characters in line following >

--- contigs2length.py
+++ contigs2length.py
@@ -3,27 +3,22 @@
 import argparse
 import sys
 
-def extract_name_length_readcount (filein):
+def extract_name_length (filein):
     filein.seek(0)
     contigs2length = {}
-    contigs2readcount = {}
     for line in filein:
         line = line.rstrip('\n')
         if line.startswith(">"):
             values = line.split(" ")
-            if not len(values) == 3:
-                    print("ERROR: Contig file is not in the correct format.")
-                    sys.exit(1)
             contig_name = values[0].split(">")[1]
-            length = values[1].split("length_")[1]
-            read_count = values[2].split("read_count_")[1]
+        else:
+            length = len(line)
             contigs2length[contig_name] = length
-            contigs2readcount[contig_name] = read_count
-    return(contigs2length,contigs2readcount)
+    return(contigs2length)
     
 if __name__ == '__main__':
     '''
-    This program will take a FASTA file of contigs (outputed by IDBA-UD) and extract the name, length, and read counts. The format of the identifier is as follows ">ContigName length read_count". For example: ">contig-95_2 length_33501 read_count_391116".
+    This program will take a FASTA file of contigs and extract the name and length. The format of the identifier is as follows ">ContigName ...". For example: ">contig-95_2 [followed by any or no text]". 
     '''
 
     parser = argparse.ArgumentParser(description='Contig Names to Length and Read Count.')
@@ -32,9 +27,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    (contigs2length,contigs2readcount) = extract_name_length_readcount(args.contigFile)
-    print "ContigName\tLength\tReadCount"
+    contigs2length = extract_name_length(args.contigFile)
+    print "ContigName\tLength"
     for contig, length in contigs2length.items():
-        readcount = contigs2readcount[contig]
-        print contig + "\t" + length + "\t" + readcount
-        
+        print contig + "\t" + str(length)
